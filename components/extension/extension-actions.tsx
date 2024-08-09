3a2cf4a4f20114b7f24e1169c8eac210ba13be2f
NotLester <lester.lewisudupi@gmail.com>
2024-08-09 18:48:12 +0530

    more restructuring

--- components/extension/extension-actions.tsx
+++ components/extension/extension-actions.tsx
@@ -1,27 +1,19 @@
-import { useExtension } from "@/contexts/extension-context"
 import { useCopyToClipboard } from "@/lib/hooks/use-copy-to-clipboard"
 import {
-  ActivityLogIcon,
   CardStackPlusIcon,
-  CaretSortIcon,
-  ChatBubbleIcon,
-  CheckIcon,
-  Link2Icon,
-  Pencil2Icon
+  CaretSortIcon, CheckIcon,
+  Link2Icon
 } from "@radix-ui/react-icons"
 
 import { Button } from "../ui/button"
 import { CollapsibleTrigger } from "../ui/collapsible"
-import { TooltipProvider } from "../ui/tooltip"
 import { TooltipWrapper } from "../ui/tooltip-wrapper"
+import ExtensionPanelButton from "./extension-panel-button"
 
 export default function ExtensionActions() {
-  const { setExtensionPanel, extensionIsOpen, setExtensionIsOpen } =
-    useExtension()
-
   const { isCopied, copyToClipboard } = useCopyToClipboard({ timeout: 2000 })
 
-  function CopyVideoURL() {
+  const CopyVideoURL = () => {
     if (isCopied) return
     copyToClipboard(window.location.href)
   }
@@ -31,39 +23,9 @@ export default function ExtensionActions() {
       <CardStackPlusIcon className="h-6 w-6 opacity-50 ml-2" />
       <div className="flex justify-center items-center space-x-2">
         <div className="flex -space-x-px">
-          <Button
-            variant="outline"
-            onClick={() => {
-              setExtensionPanel("Summary")
-              if (!extensionIsOpen) setExtensionIsOpen(true)
-            }}
-            className="
-          rounded-r-none focus:z-10 bg-transparent space-x-2 items-center">
-            <Pencil2Icon className="h-4 w-4 opacity-60" />
-            <span className="opacity-90">Summary</span>
-          </Button>
-          <Button
-            variant="outline"
-            onClick={() => {
-              setExtensionPanel("Transcript")
-              if (!extensionIsOpen) setExtensionIsOpen(true)
-            }}
-            className="
-          rounded-r-none focus:z-10 bg-transparent space-x-2 items-center">
-            <ActivityLogIcon className="h-4 w-4 opacity-60" />
-            <span className="opacity-90">Transcript</span>
-          </Button>
-          <Button
-            variant="outline"
-            onClick={() => {
-              setExtensionPanel("Chat")
-              if (!extensionIsOpen) setExtensionIsOpen(true)
-            }}
-            className="
-          rounded-r-none focus:z-10 bg-transparent space-x-2 items-center">
-            <ChatBubbleIcon className="h-4 w-4 opacity-60" />
-            <span className="opacity-90">Chat</span>
-          </Button>
+          <ExtensionPanelButton extensionType="Summary" />
+          <ExtensionPanelButton extensionType="Transcript" />
+          <ExtensionPanelButton extensionType="Chat" />
         </div>
       </div>
 
---
+++ components/extension/extension-panel-button.tsx
@@ -0,0 +1,29 @@
+import { ActivityLogIcon, ChatBubbleIcon, Pencil2Icon } from "@radix-ui/react-icons";
+import { Button } from "../ui/button";
+import { useExtension } from "@/contexts/extension-context";
+
+interface ExtensionPanelButtonProps {
+    extensionType: "Chat" | "Transcript" | "Summary"
+}
+
+export default function ExtensionPanelButton({extensionType}: ExtensionPanelButtonProps) {
+    const {setExtensionPanel, extensionIsOpen, setExtensionIsOpen} = useExtension();
+
+    const handleButtonClick = (panel: string) => {
+        setExtensionPanel(panel)
+        if (!extensionIsOpen) setExtensionIsOpen(true)
+    }
+
+    return (
+        <Button
+            variant="outline"
+            onClick={() => handleButtonClick(extensionType)}
+            className="
+          rounded-r-none focus:z-10 bg-transparent space-x-2 items-center">
+            {extensionType === "Summary" && <Pencil2Icon className="h-4 w-4 opacity-60" />}
+            {extensionType === "Transcript" && <ActivityLogIcon className="h-4 w-4 opacity-60" />}
+            {extensionType === "Chat" && <ChatBubbleIcon className="h-4 w-4 opacity-60" />}
+            <span className="opacity-90">Summary</span>
+        </Button>
+    )
+}
